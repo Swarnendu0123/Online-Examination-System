556cfe10ddad5f12e4cf058ee216e5201a9f406c
Swarnendu <clickswarnendu123@gmail.com>
2023-09-12 18:24:59 +0530

    ...

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const PORT = process.env.PUBLIC_PORT || 4000;
 const questions = require("./data.json");
 
 app.set("viwe engine", "ejs");
-
-app.get("/exam/:examId/question/:questionId", (req, res) => {
-  let questionId = req.params.questionId;
-  let ques = questions[questionId - 1];
-  console.log(ques);
-  res.render("exam.ejs", {
-    question_id: ques.question_id,
-    sub: ques.subject,
-    question: ques.question,
-    ans1: ques.option1,
-    ans2: ques.option2,
-    ans3: ques.option3,
-    ans4: ques.option4,
-    type: ques.type,
-    pos_marks: ques.pos_marks,
-    neg_marks: ques.neg_marks,
-    exam_name: ques.exam_name,
+try {
+  app.get("/exam/:examId/question/:questionId", (req, res) => {
+    let questionId = req.params.questionId;
+    let ques = questions[questionId - 1];
+    console.log(ques);
+    res.render("exam.ejs", { 
+      ques:ques,
+      questions:questions, 
+    });
   });
-});
+} catch (error) {
+  console.log(error);
+}
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+try { 
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
